2494ea95082caee3d5edf57297e83f94ccab86e1
Wayne-MBP <506110230@qq.com>
2021-04-08 23:22:21 +0800

    add eureka

---
+++ learn-springcloud/learn-springcloud-eureka/src/main/java/com/a3fun/learn/springcloud/EurekaServer.java
@@ -0,0 +1,15 @@
+package com.a3fun.learn.springcloud;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author haixiangchen
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
