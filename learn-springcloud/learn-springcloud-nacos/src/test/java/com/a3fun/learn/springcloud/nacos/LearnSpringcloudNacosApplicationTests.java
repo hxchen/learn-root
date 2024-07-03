911e7bfbfed4bdaaa59290675eb1c60ea8167e4c
hxchen <helloworld.vip@gmail.com>
2024-07-03 20:12:28 +0800

    add new files

--- learn-springcloud/learn-springcloud-nacos/src/test/java/com/a3fun/learn/springcloud/nacos/LearnSpringcloudNacosApplicationTests.java
+++ learn-springcloud/learn-springcloud-nacos/src/test/java/com/a3fun/learn/springcloud/nacos/LearnSpringcloudNacosApplicationTests.java
@@ -1,6 +1,6 @@
 package com.a3fun.learn.springcloud.nacos;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
